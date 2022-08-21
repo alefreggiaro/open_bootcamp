90ac0e63d0d1c73a590567b2ba2fa0045c792ebf
alefreggiaro <79095235+alefreggiaro@users.noreply.github.com>
2022-08-21 15:17:33 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    var resultado = suma(1,2,3);
+        System.out.println(resultado);
+
+}
+    public static int suma (int a,int b,int c){
+        return a + b + c;
+    }
+}
